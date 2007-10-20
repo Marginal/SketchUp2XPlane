5c4582815ef139d8329647a7fe167a4893ff674e
Jonathan Harris <x-plane@marginal.org.uk>
2007-10-20 19:14:38 +0000

    *** empty log message ***

--- install.cmd
+++ install.cmd
@@ -1,5 +1,5 @@
 @echo off
-copy SU2XPlane*.html	"C:\Program Files\Google\Google SketchUp\Plugins\"
-copy SU2XPlane*.html	"C:\Program Files\Google\Google SketchUp 6\Plugins\"
-copy SU2XPlane.rb	"C:\Program Files\Google\Google SketchUp\Plugins\"
-copy SU2XPlane.rb	"C:\Program Files\Google\Google SketchUp 6\Plugins\"
+copy SU2XPlane*.html "C:\Program Files\Google\Google SketchUp\Plugins\"   >nul:
+copy SU2XPlane*.html "C:\Program Files\Google\Google SketchUp 6\Plugins\" >nul:
+copy SU2XPlane.rb    "C:\Program Files\Google\Google SketchUp\Plugins\"   >nul:
+copy SU2XPlane.rb    "C:\Program Files\Google\Google SketchUp 6\Plugins\" >nul:
