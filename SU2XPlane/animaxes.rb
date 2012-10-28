46bb0fb6c33023a8e8486127608065e95a3dac56
Jonathan Harris <x-plane@marginal.org.uk>
2012-10-28 02:55:53 +0000

    Apply Change Axes fixup to all instances of the affected component.

--- SU2XPlane/animaxes.rb
+++ SU2XPlane/animaxes.rb
@@ -42,21 +42,21 @@ class XPlaneToolsObserver < Sketchup::ToolsObserver
 
   def initialize(model)
     @model=model
-    @component=nil	# component that is having its axes changed
+    @definition=nil	# ComponentDefinition of the component that is having its axes changed
     @model.tools.add_observer(self)
   end
 
   def onActiveToolChanged(tools, tool_name, tool_id)
-    puts "onActiveToolChanged #{tool_name}"
+    #puts "onActiveToolChanged #{tool_name}"
     if !@model.valid?		# this can't happen
       @model.tools.remove_observer(self)
     elsif tool_id==21126
       # Change Axes tool
       c=@model.selection.first	# can be nil if the user is changing global axes
       if c && c.typename=='ComponentInstance' && c.XPCountFrames>0
-        @component=c
-        @component.XPSavedTransformation=@component.transformation
-        @component.definition.entities.each do |c|
+        @definition=c.definition
+        @definition.instances.each { |c| c.XPSavedTransformation=c.transformation }
+        @definition.entities.each do |c|
           if c.typename=='ComponentInstance' && c.XPCountFrames>0
             (0...c.XPCountFrames).each do |frame|
               c.XPSavedTransformation=c.transformation
@@ -64,20 +64,20 @@ class XPlaneToolsObserver < Sketchup::ToolsObserver
           end
         end
       else
-        @component=nil
+        @definition=nil
       end
-    elsif @component && @component.XPSavedTransformation.to_a!=@component.transformation.to_a	# Transformation has no comparison operators
+    elsif @definition && @definition.instances.first.XPSavedTransformation.to_a!=@definition.instances.first.transformation.to_a	# Transformation has no comparison operators
       # Change Axes tool finished and axes were changed
       @model.start_operation('Change Axes', true, false, true)	# Merge with the Change Axes tool's operation
-      # replicate axes shift in stored keyframe positions
-      #shift=@model.edit_transform.inverse * Geom::Transformation.translation(@component.transformation.origin-@transformation.origin)
-      shift=@model.edit_transform.inverse * @component.transformation * @component.XPSavedTransformation.inverse
-      (0...@component.XPCountFrames).each do |frame|
-        @component.set_attribute(SU2XPlane::ATTR_DICT, SU2XPlane::ANIM_MATRIX_+frame.to_s, (shift * Geom::Transformation.new(@component.get_attribute(SU2XPlane::ATTR_DICT, SU2XPlane::ANIM_MATRIX_+frame.to_s))).to_a)
+      # replicate axes shift in stored keyframe positions for all instances
+      @definition.instances.each do |c|
+        shift=(@model.active_entities.include?(c) ? @model.edit_transform.inverse * c.transformation : c.transformation) * c.XPSavedTransformation.inverse
+        (0...c.XPCountFrames).each do |frame|
+          c.set_attribute(SU2XPlane::ATTR_DICT, SU2XPlane::ANIM_MATRIX_+frame.to_s, (shift * Geom::Transformation.new(c.get_attribute(SU2XPlane::ATTR_DICT, SU2XPlane::ANIM_MATRIX_+frame.to_s))).to_a)
+        end
       end
       # shift immediate childrens' stored keyframe positions too
-      #unshift=Geom::Transformation.translation(@transformation.origin-@component.transformation.origin)
-      @component.definition.entities.each do |c|
+      @definition.entities.each do |c|
         if c.typename=='ComponentInstance' && c.XPCountFrames>0
           shift=c.transformation * c.XPSavedTransformation.inverse
           (0...c.XPCountFrames).each do |frame|
@@ -86,10 +86,10 @@ class XPlaneToolsObserver < Sketchup::ToolsObserver
         end
       end
       @model.commit_operation
-      @component=nil
+      @definition=nil
     else
       # Some other tool, or Change Axes tool cancelled
-      @component=nil
+      @definition=nil
     end
   end
 
